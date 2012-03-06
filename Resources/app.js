a618e64c87c26e4b77bef39f2b821f6e994e36e7
tonylukasavage <anthony.lukasavage@gmail.com>
2012-03-06 16:08:12 -0500

    re-added comments and version check

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,20 @@
+/*
+* A master detail view, utilizing a native table view component and platform-specific UI and navigation. 
+* A starting point for a navigation-based application with hierarchical data, or a stack of windows. 
+* Requires Titanium Mobile SDK 1.8.0+.
+* 
+* In app.js, we generally take care of a few things:
+* - Bootstrap the application with any data we need
+* - Check for dependencies like device type, platform version or network connection
+* - Require and open our top-level UI component
+*  
+*/
+
+//bootstrap and check dependencies
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
+}
+
 // This is a single context application with mutliple windows in a stack
 (function() {
 	//determine platform and form factor and render approproate components
@@ -6,7 +23,8 @@
 		height = Ti.Platform.displayCaps.platformHeight,
 		width = Ti.Platform.displayCaps.platformWidth;
 	
-	//considering tablet to have one dimension over 900px - can define your own
+	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
+	//yourself what you consider a tablet form factor for android
 	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
 	
 	var Window;
